3a89fc76eba9fc70be3b1d213e6d3fa61fbb95f7
AhmedBnKhalil <122465822+AhmedBnKhalil@users.noreply.github.com>
2023-05-19 02:54:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def predict():
     int_features = [x for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+
     if prediction == 1:
-        prediction= 'Positive'
-    elif prediction ==0:
-        prediction= 'Negative'
+        prediction ='POSITIVE'
+    elif prediction == 0:
+        prediction = 'NEGATIVE'
 
     return render_template('index.html', prediction_text="This person prediction is {}".format(prediction))
 
